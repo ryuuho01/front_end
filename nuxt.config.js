87312ed5bd95f0aaea2df1ee4cb10822d56d30ca
ryuuho01 <ryuuho02@gmail.com>
2022-02-21 20:13:11 +0900

    title_change

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'additional_advance-front',
+    title: 'Rese',
     htmlAttrs: {
       lang: 'ja'
     },
